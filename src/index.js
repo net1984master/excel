56450951dc917a66c93a9fc9d1c53425e3131aa2
Alexander Khodzinsky <net1984master@gmail.com>
2023-05-15 16:19:12 +0300

    css подготовка болванок для классов

--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
 import './scss/index.scss';
+import {Excel} from '@/components/excel/Excel';
 console.log('Working!');
+
+const excel = new Excel('#app', {
+    components: [],
+});
+
+console.log(excel);
